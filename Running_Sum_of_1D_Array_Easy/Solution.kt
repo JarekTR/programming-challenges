cad4802f1b7c91a5cfa72b542ef04120c41a3238
JarekTC <carlson.jarek@gmail.com>
2021-06-07 19:37:28 -0600

    Update file naming convention

--- Running_Sum_of_1D_Array_Easy/Solution.kt
+++ Running_Sum_of_1D_Array_Easy/Solution.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun runningSum(nums: IntArray): IntArray {
-        for(i in 1..nums.size - 1) {
+        for (i in 1..nums.size - 1) {
             nums[i] += nums[i - 1]
-        } 
+        }
         return nums
     }
 }

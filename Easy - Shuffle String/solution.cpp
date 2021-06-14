84cf9b12b71e1b8c86e70be838e0ee494ac457da
JarekTC <carlson.jarek@gmail.com>
2021-06-14 16:42:12 -0600

    Add Shuffle String C++ solution to repo

---
+++ Easy - Shuffle String/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int x;
+        std::string retString = s;
+        for(int i = 0; i < s.size(); i++) {
+            x = indices[i];
+            retString[x] = s[i];
+        }
+        return retString;
+    }
+};
+

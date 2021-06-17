5d59ebdd1648972fcc0e785762ca242fef69e057
JarekTC <carlson.jarek@gmail.com>
2021-06-17 09:13:52 -0600

    Add Remove vowels from a string C solution to repo

---
+++ 1119 - Easy - Remove vowels from a string/solution.c
@@ -0,0 +1,19 @@
+char * removeVowels(char * s) {
+    int c = 0;
+    int size = strlen(s);
+    char *ret = malloc(size);
+    for (int i = 0; i < size; i++) {
+        if (
+            s[i] != 'a' &&
+            s[i] != 'e' &&
+            s[i] != 'i' &&
+            s[i] != 'o' &&
+            s[i] != 'u'
+        ) {
+            ret[c] = s[i];
+            c++;
+        }
+    }
+    ret[c] = '\0';
+    return ret;
+}

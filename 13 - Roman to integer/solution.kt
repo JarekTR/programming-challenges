9fd4e197629ac738e2299f38cf0966183a4c3081
JarekTC <carlson.jarek@gmail.com>
2021-07-02 15:52:03 -0600

    Update solution.kt

--- 13 - Roman to integer/solution.kt
+++ 13 - Roman to integer/solution.kt
@@ -1,16 +1,16 @@
-class Solution {
+class solution {
     fun romanToInt(s: String): Int {
         var count = 0
         var prev = ' '
         for (i in s.length - 1 downTo 0) {
             count += when (s[i]) {
-                'I' -> if (prev == 'V' || prev == 'X') -1 else 1
-                'X' -> if (prev == 'L' || prev == 'C') -10 else 10
-                'C' -> if (prev == 'D' || prev == 'M') -100 else 100
-                'V' -> 5
-                'L' -> 50
-                'D' -> 500
+                'C' -> when (prev) { 'D', 'M' -> -100 else -> 100 } 
+                'X' -> when (prev) { 'L', 'C' -> -10 else -> 10 }
+                'I' -> when (prev) { 'V', 'X' -> -1 else -> 1 }
                 'M' -> 1000
+                'D' -> 500
+                'L' -> 50
+                'V' -> 5
                 else -> { 0 }
             }
             prev = s[i]

5f4033fbda90ed380479fb7c733e6cab0a3c3c46
JarekTC <carlson.jarek@gmail.com>
2021-07-05 11:15:39 -0600

    Update solution.kt

--- 13 - Roman to integer/solution.kt
+++ 13 - Roman to integer/solution.kt
@@ -4,9 +4,18 @@ class solution {
         var prev = ' '
         for (i in s.length - 1 downTo 0) {
             count += when (s[i]) {
-                'I' -> when (prev) { 'V', 'X' -> -1 else -> 1 }
-                'X' -> when (prev) { 'L', 'C' -> -10 else -> 10 }
-                'C' -> when (prev) { 'D', 'M' -> -100 else -> 100 } 
+                'I' -> when (prev) { 
+                    'V', 'X' -> -1 
+                    else -> 1 
+                }
+                'X' -> when (prev) { 
+                    'L', 'C' -> -10 
+                    else -> 10 
+                }
+                'C' -> when (prev) { 
+                    'D', 'M' -> -100 
+                    else -> 100 
+                } 
                 'V' -> 5
                 'L' -> 50
                 'D' -> 500
